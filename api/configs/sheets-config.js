77830c77af0f5434ac049db9dc57bc1c5d15ed1d
AlanSalviano <alanqms@gmail.com>
2025-09-21 21:07:13 -0300

    Add files via upload

---
+++ api/configs/sheets-config.js
@@ -0,0 +1,7 @@
+// api/configs/sheets-config.js
+
+export const SHEET_NAME_APPOINTMENTS = 'Datatest';
+export const SHEET_NAME_EMPLOYEES = 'Employees';
+export const SHEET_NAME_FRANCHISES = 'Regions';
+export const SHEET_NAME_USERS = 'Users';
+export const SHEET_NAME_ROLES = 'Roles';
